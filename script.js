1864e4b3f6de9772752cbcd489f203fb78b3512f
Ashvin Tiwari <ashvintiwari161@gmail.com>
2026-03-18 09:22:23 +0530

    feat: display breed name

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ async function fetchDogImage() {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const data = await response.json();
     const imageUrl = data.message;
+    const breedName = imageUrl.split('/')[4];
 
     document.getElementById("dogImage").src = imageUrl;
+    document.querySelector(".box h3").textContent = breedName;
 
     clickCount++;
     document.getElementById("clickCount").textContent = clickCount;
